545f74e9507b4e99ea2b8519394d75ea195546d3
boubbin@gmail.com <boubbin@gmail.com@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-09-16 08:09:24 +0000

    lisätty IndexController ui pakettiin

---
+++ trunk/merkkipaivat/src/ui/IndexController.java
@@ -0,0 +1,28 @@
+package ui;
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@SuppressWarnings({ "serial", "unused" })
+public class IndexController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
