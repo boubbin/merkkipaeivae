e0346968342dc0e6eac98932231e094819313a42
boubbin@gmail.com <boubbin@gmail.com@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-09-19 08:06:13 +0000

    poistetu default package ja muokattu index kontrolleria

--- merkkipaivat/src/helloworld.java
+++
@@ -1,8 +0,0 @@
-
-public class helloworld {
-
-	private String heihei;
-	public void moikkamoi() {
-		System.out.println(heihei);
-	}
-}
--- merkkipaivat/src/ui/IndexController.java
+++ merkkipaivat/src/ui/IndexController.java
@@ -1,10 +1,12 @@
 package ui;
 
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,15 +16,19 @@ import javax.servlet.http.HttpSession;
 public class IndexController extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	    //ServletContext context = getServletContext();
+	    //RequestDispatcher dispatcher = context.getRequestDispatcher("/jsp/index.jsp");
+	    //dispatcher.forward(req, resp);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-	}
+	    //ServletContext context = getServletContext();
+	    //RequestDispatcher dispatcher = context.getRequestDispatcher("/jsp/index.jsp");
+	    //dispatcher.forward(req, resp);
 
+	}
 }
