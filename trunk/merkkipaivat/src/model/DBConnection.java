e7808eb158ca80f9119e9fc5b2e7108b4dd8466e
aaltopv@gmail.com <aaltopv@gmail.com@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-09-20 15:42:43 +0000

    luotu dbconnection.java

---
+++ trunk/merkkipaivat/src/model/DBConnection.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBConnection {
+
+	private Connection connection;
+	
+	public DBConnection()
+	{
+		this.connect();
+	}
+	
+	public void connect()
+	{
+		try {
+        	Context initCtx = new InitialContext();
+        	Context envCtx = (Context) initCtx.lookup("java/comp/env");
+        	DataSource ds = (DataSource) envCtx.lookup("jdbc/mysliDB");
+        	this.connection = ds.getConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}       	
+        	catch (NamingException ex)
+	        {
+	        	System.out.println(ex.getMessage());
+	        	ex.printStackTrace();
+	        }
+	}
+	
+	public void disconnect()
+	{
+		try {
+			this.connection.close();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection()
+	{
+		if(this.connection == null)
+		{
+			this.connect();
+		}
+		
+		return this.connection;
+	}
+}
