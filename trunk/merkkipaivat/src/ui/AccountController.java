6cc0efc9013b454208534df96545311feb66aa92
boubbin@gmail.com <boubbin@gmail.com@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-09-21 17:01:13 +0000

    Creating an user account is implemented now

--- trunk/merkkipaivat/src/ui/AccountController.java
+++ trunk/merkkipaivat/src/ui/AccountController.java
@@ -1,6 +1,7 @@
 package ui;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -11,20 +12,34 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import model.CreateAccountFormValidator;
+import model.DBConnection;
+import model.DBHelper;
+import model.DBQuery;
 import model.UserBean;
 
 @SuppressWarnings("serial")
 public class AccountController extends HttpServlet {
 
+	private boolean CreateUserAccount(HttpServletRequest request) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password1");
+		String email    = request.getParameter("email");
+		boolean success;
+		DBConnection conn = new DBConnection();
+		DBQuery query = new DBQuery(conn.getConnection());
+		try {
+			success = query.createNewUserAccount(username, password, email);
+		} catch (SQLException e) {
+			return false;
+		}
+		if (success) { return true; }
+		return false;
+	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession(true);
 		ServletContext context = getServletContext();
-		if(session.getAttribute("authed") == null)
-		{
-			session.setAttribute("authed", 0);
-		}
-		
+		if(session.getAttribute("authed") == null) { session.setAttribute("authed", 0); }
 		
 		if((Integer)session.getAttribute("authed") == 1)
 		{
@@ -32,6 +47,11 @@ public class AccountController extends HttpServlet {
 			{
 			    RequestDispatcher dispatcher = context.getRequestDispatcher("/jsp/account/edit.jsp");
 			    dispatcher.forward(req, resp);
+			} else if (req.getParameter("action").equals("logout")) {
+				session.setAttribute("authed", 0);
+				session.invalidate();
+			    RequestDispatcher dispatcher = context.getRequestDispatcher("/mainpage.jsp");
+			    dispatcher.forward(req, resp);				
 			}
 			else
 			{
@@ -49,12 +69,14 @@ public class AccountController extends HttpServlet {
 					// the data is valid, but is not converted or sanitized!
 					// so save it to mysql and make sure to use prepared statements
 					session.setAttribute("account_created_ok", "1");
+					if (this.CreateUserAccount(req)) { session.setAttribute("account_created_ok", "1"); }
+					else { session.setAttribute("account_created_ok", "0"); }
 				} else {
 					// the data wasn't valid, it was submitted tho!
 				}
 			} else {
-				// First time here, do we ened to do anything special? no?
-				// well reset some MESSAGES so they dont appear as null
+				// First time here, do we need to do anything special? no?
+				// well reset some MESSAGES so they don't appear as null
 				session.setAttribute("usernameMessage", " ");
 				session.setAttribute("passwordMessage", " ");
 				session.setAttribute("emailMessage", " ");
