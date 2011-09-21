57c10c5f770cebf8376801d5a0cf91206e3331e1
aaltopv@gmail.com <aaltopv@gmail.com@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-09-21 07:38:40 +0000

    anniver controllol

--- merkkipaivat/src/ui/AnniversaryController.java
+++ merkkipaivat/src/ui/AnniversaryController.java
@@ -49,6 +49,7 @@ public class AnniversaryController extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	       HttpSession session = req.getSession(true);
 		
+	       
 	}
 
 	
