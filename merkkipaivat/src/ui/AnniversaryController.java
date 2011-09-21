a1404f326a5ffb56604536f6178041f047564499
aaltopv@gmail.com <aaltopv@gmail.com@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-09-21 07:26:56 +0000

    anniversarycontroller lisätty

---
+++ merkkipaivat/src/ui/AnniversaryController.java
@@ -0,0 +1,55 @@
+package ui;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AnniversaryController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	       HttpSession session = req.getSession(true);
+	       ServletContext context = getServletContext();
+	       
+	       if((Integer)session.getAttribute("authed") == 1)
+	       {
+	    	   if(req.getParameter("action").equals("all"))
+	    	   {
+	    		   //näytä kaikki merkkipäivät
+					RequestDispatcher dispatcher = context.getRequestDispatcher("/jsp/anniversary/all.jsp");
+				    dispatcher.forward(req, resp);
+	    	   }
+	    	   else if(req.getParameter("action").equals("edit"))
+	    	   {
+	    		   //editoidaan yhtä
+					RequestDispatcher dispatcher = context.getRequestDispatcher("/jsp/anniversary/edit.jsp");
+				    dispatcher.forward(req, resp);
+	    	   }
+	    	   else if(req.getParameter("action").equals("create"))
+	    	   {
+	    		   //luo uusi merkkipäivä
+					RequestDispatcher dispatcher = context.getRequestDispatcher("/jsp/anniversary/create.jsp");
+				    dispatcher.forward(req, resp);	    		   
+	    	   }
+	       }
+	       else
+	       {
+		   	    RequestDispatcher dispatcher = context.getRequestDispatcher("/mainpage.jsp");
+			    dispatcher.forward(req, resp);	    	   
+	       }
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	       HttpSession session = req.getSession(true);
+		
+	}
+
+	
+}
