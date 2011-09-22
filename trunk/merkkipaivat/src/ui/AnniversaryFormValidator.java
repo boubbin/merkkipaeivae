1145d7fb4792b88d8336de04de860664b65831fe
aaltopv@gmail.com <aaltopv@gmail.com@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-09-22 13:00:51 +0000

    anniversaryfromvalidator yesyes

--- trunk/merkkipaivat/src/ui/AnniversaryFormValidator.java
+++ trunk/merkkipaivat/src/ui/AnniversaryFormValidator.java
@@ -18,10 +18,31 @@ public class AnniversaryFormValidator {
 	}
 
 	private boolean validateDate(String date) {
+		String[] tokens = date.split("-");
+		try {
+			if (tokens[0] == null || tokens[1] == null || tokens[2] == null) 	{ return false; }
+			if (!this.validateInt(tokens[0])) 									{ return false; }
+			if (!this.validateInt(tokens[1])) 									{ return false; }
+			if (!this.validateInt(tokens[2])) 									{ return false; }
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
 		return true;
 	}
 
 	private boolean validateName(String name) {
+		if(name.length() < 2 || name.length() > 30)
+		{ return false; }
 		return true;
+	}
+	
+	private boolean validateInt(String integer) {
+		if (integer == null) { return false; }
+		try {
+			Integer.parseInt(integer);
+			return true;
+		} catch(NumberFormatException nfe) {
+			return false;
+		}
 	}
 }
