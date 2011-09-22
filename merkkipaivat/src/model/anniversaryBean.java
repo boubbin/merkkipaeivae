53a5e15370989597e59bfc4c6be604b56b712563
aaltopv@gmail.com <aaltopv@gmail.com@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-09-22 12:01:48 +0000

    sitä hyvää paskaa eli anniversaryy

--- merkkipaivat/src/model/anniversaryBean.java
+++ merkkipaivat/src/model/anniversaryBean.java
@@ -4,7 +4,9 @@ package model;
 public class anniversaryBean implements java.io.Serializable {
 
 	private String name;
-	private String pvm;	
+	private String pvm;
+	private int userid;
+	private int id;
 	
 	public anniversaryBean() 
 	{
@@ -25,6 +27,22 @@ public class anniversaryBean implements java.io.Serializable {
 
 	public void setPvm(String pvm) {
 		this.pvm = pvm;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 
