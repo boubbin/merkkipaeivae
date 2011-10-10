fd5dc2ef4b4960ccec3023818330cbb8ad4813bb
aaltopv <aaltopv@938e3f35-95dc-38a1-87c5-0dfcd456e5da>
2011-10-10 07:27:20 +0000

    pieni

--- trunk/Sprinkipaivat/src/sprinki/paivat/com/controller/IndexController.java
+++ trunk/Sprinkipaivat/src/sprinki/paivat/com/controller/IndexController.java
@@ -2,12 +2,14 @@ package sprinki.paivat.com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping(value={"/mainpage", "/"})
 public class IndexController{
 
-	@RequestMapping(value={"/mainpage", "/"})
+	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView index()
 	{
 		return new ModelAndView("mainpage");
